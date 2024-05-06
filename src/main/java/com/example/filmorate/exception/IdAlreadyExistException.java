eff4de9ff6e28053b0b49cfc9d6b3092d51c44b3
Alexey Akulich <loki87.666@gmail.com>
2024-05-06 17:09:37 +0300

    feat: add validation

--- src/main/java/com/example/filmorate/exception/IdAlreadyExistException.java
+++
@@ -1,8 +0,0 @@
-package com.example.filmorate.exception;
-
-public class IdAlreadyExistException extends Exception {
-
-    public IdAlreadyExistException(String message) {
-        super(message);
-    }
-}
