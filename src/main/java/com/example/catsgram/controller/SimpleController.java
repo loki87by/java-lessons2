d186133dded17d734b744cda5bceb4b26656a20d
Alexey Akulich <loki87.666@gmail.com>
2024-05-06 10:53:18 +0300

    feat: add lombok

--- src/main/java/com/example/catsgram/controller/SimpleController.java
+++ src/main/java/com/example/catsgram/controller/SimpleController.java
@@ -1,17 +1,20 @@
 package com.example.catsgram.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class SimpleController {
 
-    private  static final Logger log = LoggerFactory.getLogger(SimpleController.class);
     @GetMapping("/home")
-    public String homePage() {
-        log.info("Запрос получен");
+    public String homePage(HttpServletRequest req) {
+        log.info("Получен запрос к эндпоинту: '{}{}',\nСтрока параметров запроса: '{}'",
+                req.getMethod(),
+                req.getRequestURI(),
+                req.getQueryString());
         return "Котограм";
     }
 }
--- src/main/java/com/example/catsgram/model/User.java
+++ src/main/java/com/example/catsgram/model/User.java
@@ -1,34 +1,12 @@
 package com.example.catsgram.model;
 
+import lombok.*;
+
 import java.time.LocalDate;
 
+@Data
 public class User {
     private String email;
     private String nickname;
     private LocalDate birthdate;
-
-    //*getters
-    public String getEmail() {
-        return email;
-    }
-    public String getNickname() {
-        return nickname;
-    }
-
-    public LocalDate getBirthdate() {
-        return birthdate;
-    }
-
-    //*setters
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public void setBirthdate(LocalDate birthdate) {
-        this.birthdate = birthdate;
-    }
 }
